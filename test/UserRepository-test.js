497ddbcee588c3a60feeadafa1aa05add92b7a90
Hayley Dixon <hdixon37@gmail.com>
2021-07-12 22:05:14 -0600

    Run atom beatifier for formating

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+import {
+  expect
+} from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
@@ -12,27 +14,29 @@ describe('User Repository', () => {
         "email": "Diana.Hayes1@hotmail.com",
         "strideLength": 4.3,
         "dailyStepGoal": 10000,
-        "friends": [ 16, 4, 8 ]},
+        "friends": [16, 4, 8]
+      },
 
-        {
-          "id": 2,
-          "name": "Jarvis Considine",
-          "address": "30086 Kathryn Port, Ciceroland NE 07273",
-          "email": "Dimitri.Bechtelar11@gmail.com",
-          "strideLength": 4.5,
-          "dailyStepGoal": 5000,
-          "friends": [9, 18, 24, 19]
-      }])
+      {
+        "id": 2,
+        "name": "Jarvis Considine",
+        "address": "30086 Kathryn Port, Ciceroland NE 07273",
+        "email": "Dimitri.Bechtelar11@gmail.com",
+        "strideLength": 4.5,
+        "dailyStepGoal": 5000,
+        "friends": [9, 18, 24, 19]
+      }
+    ])
   })
 
 
-  it('should be a function', function () {
+  it('should be a function', function() {
     expect(UserRepository).to.be.a('function');
   });
 
   it('should be an instance of the user repository', () => {
     expect(userRepo).to.be.an.instanceof(UserRepository);
-});
+  });
 
   it('should show user data by ID', () => {
     expect(userRepo.getUserbyID(1)).to.equal(userRepo[0])
