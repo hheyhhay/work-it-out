93775b3d215d77dc89da6674ebbd5dd084798352
Hayley Dixon <hdixon37@gmail.com>
2021-07-14 14:49:15 -0600

    Connecing Webpack bundler to files

--- src/scripts.js
+++ src/scripts.js
@@ -13,3 +13,5 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+
+console.log('HELLO?')
