3713a4f2ceabfb95d65b89db2b2cc8fd04b1c4da
Hayley Dixon <hdixon37@gmail.com>
2021-07-17 13:29:26 -0600

    Write method and test for waterByDate and averageWater

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,7 +1,46 @@
 class Hydration {
-  constructor( ) {
-
+  constructor(hydrationData) {
+    this.hydroData = hydrationData;
   }
+  averageWater(userID){
+    let foundUser = this.hydroData.filter((user) => { // could be built in as helper function?
+      return user.userID === userID;
+    })
+
+
+    let averageWater = foundUser.reduce((acc, currentValue) => {
+      acc = acc + currentValue.numOunces;
+      return acc;
+    }, 0)
+
+    averageWater = Number((averageWater/foundUser.length).toFixed(1));
+
+    return averageWater;
+  }
+
+    waterByDate(date, userID) {
+
+      let foundUser = this.hydroData.filter((user) => user.userID === userID ).find((user) => user.date === date)
+
+      return foundUser.numOunces;
+    };
+
+    weeklyWaterTotal(date, userID) {
+      // the date, and the 6 previous dates, stored in an... aray of objects?
+      // key of date value of amount of waterByDate
+      // I believe use map since we are returning an array of the same amount,
+
+      let foundUser = this.hydroData.filter((user) => user.userID === userID).map((user) => {
+        let newObj = {};
+        newObj['date'] = user.date;
+        newObj['numOunces'] = user.numOunces;
+        return newObj;
+      })
+      return foundUser
+    }
+
+
 };
+
 
 module.exports = Hydration;
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,3 +2,94 @@ import { expect } from 'chai';
 import Sleep from '../src/Sleep';
 
 const Hydration = require('../src/Hydration')
+
+describe.only('Hydration', () => {
+
+  let hydrationData;
+  let hydration;
+
+  beforeEach(() => {
+    hydrationData = [
+      {userID: 1, date: "2019/06/15", numOunces: 37},
+      {userID: 1, date: "2019/06/16", numOunces: 69},
+      {userID: 1, date: "2019/06/17", numOunces: 96},
+      {userID: 1, date: "2019/06/18", numOunces: 61},
+      {userID: 1, date: "2019/06/19", numOunces: 91},
+      {userID: 1, date: "2019/06/20", numOunces: 50},
+      {userID: 1, date: "2019/06/21", numOunces: 50},
+      {userID: 2, date: "2019/06/15", numOunces: 75},
+      {userID: 2, date: "2019/06/16", numOunces: 91},
+      {userID: 2, date: "2019/06/17", numOunces: 96},
+      {userID: 2, date: "2019/06/18", numOunces: 70},
+      {userID: 2, date: "2019/06/19", numOunces: 76},
+      {userID: 2, date: "2019/06/20", numOunces: 71},
+      {userID: 2, date: "2019/06/21", numOunces: 27}
+    ]
+    hydration = new Hydration(hydrationData)
+  });
+
+  it('should intantiate with an array of objects', () => {
+
+
+    expect(hydration).to.be.an.instanceOf(Hydration);
+  });
+
+  it('should find average fluid ounces per day for a specified user', () => {
+
+    expect(hydration.averageWater(1)).to.equal(64.9);
+  });
+
+  it('should find amount of fluid ounces consumed on specific date', () => {
+     expect(hydration.waterByDate("2019/06/19", 2)).to.equal(76);
+  });
+
+  it('should be able keep track of how much water is drinken each day for a week', () => {
+    expect(hydration.weeklyWaterTotal("2019/06/21", 1)).to.deep.equal([
+          { date: "2019/06/15", numOunces: 37},
+          { date: "2019/06/16", numOunces: 69},
+          { date: "2019/06/17", numOunces: 96},
+          { date: "2019/06/18", numOunces: 61},
+          { date: "2019/06/19", numOunces: 91},
+          { date: "2019/06/20", numOunces: 50},
+          { date: "2019/06/21", numOunces: 50}
+        ])
+  })
+
+  it('should be able to keep track with a larger data set', () => {
+    hydrationData = [
+      {userID: 1, date: "2019/06/15", numOunces: 37},
+      {userID: 1, date: "2019/06/16", numOunces: 69},
+      {userID: 1, date: "2019/06/17", numOunces: 96},
+      {userID: 1, date: "2019/06/18", numOunces: 61},
+      {userID: 1, date: "2019/06/19", numOunces: 91},
+      {userID: 1, date: "2019/06/20", numOunces: 50},
+      {userID: 1, date: "2019/06/21", numOunces: 50},
+      {userID: 2, date: "2019/06/15", numOunces: 75},
+      {userID: 2, date: "2019/06/16", numOunces: 91},
+      {userID: 2, date: "2019/06/17", numOunces: 96},
+      {userID: 2, date: "2019/06/18", numOunces: 70},
+      {userID: 2, date: "2019/06/19", numOunces: 76},
+      {userID: 2, date: "2019/06/20", numOunces: 71},
+      {userID: 2, date: "2019/06/21", numOunces: 27},
+      {userID: 2, date: "2019/06/22", numOunces: 58},
+      {userID: 2, date: "2019/06/23", numOunces: 44},
+      {userID: 2, date: "2019/06/24", numOunces: 33}
+    ]
+
+    hydration = new Hydration(hydrationData)
+    expect(hydration.weeklyWaterTotal("2019/06/24", 2)).to.deep.equal([
+      {date: "2019/06/18", numOunces: 70},
+      {date: "2019/06/19", numOunces: 76},
+      {date: "2019/06/20", numOunces: 71},
+      {date: "2019/06/21", numOunces: 27},
+      {date: "2019/06/22", numOunces: 58},
+      {date: "2019/06/23", numOunces: 44},
+      {date: "2019/06/24", numOunces: 33}
+        ])
+
+
+  })
+
+
+
+})
