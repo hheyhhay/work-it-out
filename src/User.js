d16b63ef8df964518e41c0c5ca489e0458c3df13
Janika Hortizuela <jhortizuela21@gmail.com>
2021-07-19 23:17:58 -0600

    Remove white spaces, add semicolon

--- src/User.js
+++ src/User.js
@@ -4,15 +4,12 @@ const APIcalls = require('./apiCalls') // do we need API calls here?
 class User {
   constructor(userData) {
     this.user = userData;
-  }
+  };
 
   returnFirstName() {
-    let firstName = this.user.name.split(' ')
-    return firstName[0]
-  }
-
-
-
-}
+    let firstName = this.user.name.split(' ');
+    return firstName[0];
+  };
+};
 
 module.exports = User;
